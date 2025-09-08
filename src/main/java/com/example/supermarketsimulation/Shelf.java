74f44ba1b4bf7bc33ae803b33baa7f929f7208e4
Henson-k <hensonkorte@gmail.com>
2025-09-08 23:40:00 +0200

    added the classes, Customer, Product, Shelf

--- src/main/java/com/example/supermarketsimulation/Shelf.java
+++ src/main/java/com/example/supermarketsimulation/Shelf.java
@@ -5,4 +5,23 @@ import javafx.fxml.FXML;
 public class Shelf {
     private String shelfName;
     private String shelfProduct;
+
+    public Shelf(String shelfName, String shelfProduct) {
+        this.shelfName = shelfName;
+        this.shelfProduct = shelfProduct;
+    }
+    public String getShelfName() {
+        return shelfName;
+    }
+    public String getShelfProduct() {
+        return shelfProduct;
+    }
+
+    public void setShelfName(String shelfName) {
+        this.shelfName = shelfName;
+    }
+    public void setShelfProduct(String shelfProduct) {
+        this.shelfProduct = shelfProduct;
+    }
+
 }
